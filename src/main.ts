439f72ac0437da6263b8d7a6472396469eea57fa
omowonuola <oyedmola@gmail.com>
2023-01-21 04:38:24 +0000

    adds logger setup to the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Credit Card Processing')
+    .setDescription('Adding new credit card API description')
+    .setVersion('1.0')
+    .addTag('Cards')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  logger.log(`Application listing on port ${PORT}`);
 }
 bootstrap();
