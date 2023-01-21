149be6645a2af5726873bc093522343615da513d
omowonuola <oyedmola@gmail.com>
2023-01-21 04:03:18 +0000

    adds the create card api in the controller file

--- src/cards/cards.entity.ts
+++ src/cards/cards.entity.ts
@@ -2,4 +2,8 @@ import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
 
 export class Card implements InMemoryDBEntity {
   id: string;
+  cardName: string;
+  cardNumber: number;
+  limit: number;
+  balance: number;
 }
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateCardDto } from 'src/dto/create-card.dto';
 import { Card } from './cards.entity';
 import { CardsRepository } from './cards.repository';
 
@@ -12,5 +17,25 @@ export class CardsService {
       throw new NotFoundException('No card details found');
     }
     return found;
+  }
+
+  createCard(payload: CreateCardDto): Card {
+    const { id, cardName, cardNumber, limit } = payload;
+
+    const card: Card = {
+      id,
+      cardName,
+      cardNumber,
+      limit,
+      balance: 0,
+    };
+    // check out for duplicate card number
+    const count = this.cardsRepository.query(
+      (Card) => Card.cardNumber == cardNumber,
+    );
+    if (count.length > 0) {
+      throw new ConflictException('Card number already exists');
+    }
+    return this.cardsRepository.create(card);
   }
 }
