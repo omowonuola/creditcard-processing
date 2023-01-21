3b828d27d295387d611108b80d802b9599631c7a
omowonuola <oyedmola@gmail.com>
2023-01-21 03:00:22 +0000

    imports the mvs setup

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CardsController } from './cards.controller';
+import { CardsService } from './cards.service';
 
-@Module({})
+@Module({
+  controllers: [CardsController],
+  providers: [CardsService],
+})
 export class CardsModule {}
