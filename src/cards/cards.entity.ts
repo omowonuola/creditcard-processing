a9634ba7aca3696ce91c6e60136213e5fd4268ef
omowonuola <oyedmola@gmail.com>
2023-01-21 04:35:55 +0000

    adds swagger documentation to the codebase

--- src/cards/cards.entity.ts
+++ src/cards/cards.entity.ts
@@ -1,9 +1,30 @@
 import { InMemoryDBEntity } from '@nestjs-addons/in-memory-db';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class Card implements InMemoryDBEntity {
   id: string;
+  /**
+   * The card name
+   * @example John Wick
+   */
+  @ApiProperty({
+    example: 'John Wick',
+    description: 'The credit card name',
+  })
   cardName: string;
+
+  @ApiProperty({
+    example: 4111111111111111,
+    description: 'The credit card number',
+  })
   cardNumber: number;
+
+  @ApiProperty({ example: 500, description: 'The credit limit for the card' })
   limit: number;
+
+  @ApiProperty({
+    example: 0,
+    description: 'The initial balance for the card',
+  })
   balance: number;
 }
--- src/dto/create-card.dto.ts
+++ src/dto/create-card.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { IsCreditCardValid } from 'src/cards/cards.validator';
 
@@ -5,11 +6,19 @@ export class CreateCardDto {
   @IsOptional()
   id: string;
 
+  @ApiProperty({
+    example: 'John Wick',
+    description: 'The credit card name',
+  })
   @IsNotEmpty()
   @IsString()
   cardName: string;
 
   //   integer validation
+  @ApiProperty({
+    example: 4111111111111111,
+    description: 'The credit card number',
+  })
   @IsNotEmpty()
   @IsNumber()
   @IsCreditCardValid({
@@ -18,6 +27,7 @@ export class CreateCardDto {
   cardNumber: number;
 
   //   integer validation
+  @ApiProperty({ example: 500, description: 'The credit limit for the card' })
   @IsNotEmpty()
   @IsNumber()
   limit: number;
